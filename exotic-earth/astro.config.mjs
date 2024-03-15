7ff0d7bb2f4757a8896bdea4935d1e845196b8cc
Adrian Donato <adriandonato9@gmail.com>
2024-03-15 07:04:35 -0500

    commit

--- exotic-earth/astro.config.mjs
+++ exotic-earth/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+ import netlify from '@astrojs/netlify/functions';
+
+export default defineConfig({
+
+   adapter: netlify()
+});
